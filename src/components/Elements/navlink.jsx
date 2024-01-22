48bb4b24c62d2b7efa0e201bc277915f26537439
alipnf <alipnf@proton.me>
2024-01-22 21:44:04 +0700

    feat(Elements): add navlink.jsx

---
+++ src/components/Elements/navlink.jsx
@@ -0,0 +1,11 @@
+const Navlink = ({ text }) => {
+  return (
+    <>
+      <a href="" className="w-24 font-medium text-[#ffffffcc] hover:font-bold">
+        {text}
+      </a>
+    </>
+  );
+};
+
+export default Navlink;
