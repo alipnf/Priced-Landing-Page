37008ca7d3fcc05bee3a69d17f6a349fea79d15c
alipnf <alipnf@proton.me>
2024-01-23 19:38:57 +0700

    feat(Elements): add feature.jsx

---
+++ src/components/Elements/feature.jsx
@@ -0,0 +1,13 @@
+const Feature = ({ text, icon }) => {
+  const isCheck = icon === "check";
+  const imagePath = isCheck ? "src/assets/check.svg" : "src/assets/cross.svg";
+
+  return (
+    <div className="flex gap-2">
+      <img src={imagePath} alt={`${icon} icon`} />
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default Feature;
