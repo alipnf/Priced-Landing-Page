425bd6622ff5d8585b575ba5d391feda40df68df
alipnf <alipnf@proton.me>
2024-01-23 20:11:34 +0700

    fix(feature): directory root

--- src/components/Elements/feature.jsx
+++ src/components/Elements/feature.jsx
@@ -1,6 +1,8 @@
 const Feature = ({ text, icon }) => {
   const isCheck = icon === "check";
-  const imagePath = isCheck ? "src/assets/check.svg" : "src/assets/cross.svg";
+  const imagePath = isCheck
+    ? "./src/assets/check.svg"
+    : "./src/assets/cross.svg";
 
   return (
     <div className="flex gap-2">
