e508ac48449b0f607ae1bcb31390b18f6c1e273e
alipnf <alipnf@proton.me>
2024-01-23 19:39:55 +0700

    feat(pages): add hero.jsx

---
+++ src/pages/hero.jsx
@@ -0,0 +1,9 @@
+import CardLayout from "../components/Layouts/cardLayout";
+const Hero = () => {
+  return (
+    <main className="container mx-auto">
+      <CardLayout></CardLayout>
+    </main>
+  );
+};
+export default Hero;
