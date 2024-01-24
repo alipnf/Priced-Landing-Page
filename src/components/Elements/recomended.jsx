bcb3ec7fbba0da9417e0dedd5db653a8fd1a79ee
alipnf <alipnf@proton.me>
2024-01-24 20:27:59 +0700

    feat(Elements): add recomended.jsx

---
+++ src/components/Elements/recomended.jsx
@@ -0,0 +1,9 @@
+const Recomended = () => {
+  return (
+    <p className="mt-[-25px] rounded-t-lg bg-[#160E4B] py-[5px] text-center text-[10px] font-bold text-white">
+      Recomended
+    </p>
+  );
+};
+
+export default Recomended;
