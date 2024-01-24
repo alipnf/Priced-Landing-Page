a94c29f99fdd48b0497b52d5e74ca2436dd8a07b
alipnf <alipnf@proton.me>
2024-01-24 15:23:53 +0700

    move icon & update directory

--- src/components/Elements/feature.jsx
+++ src/components/Elements/feature.jsx
@@ -1,8 +1,6 @@
 const Feature = ({ text, icon }) => {
   const isCheck = icon === "check";
-  const imagePath = isCheck
-    ? "./src/assets/check.svg"
-    : "./src/assets/cross.svg";
+  const imagePath = isCheck ? "/icons/check.svg" : "/icons/cross.svg";
 
   return (
     <div className="flex gap-2">
